b604aced96f064b9c33ad6303282e34645401ee3
Mohammed Ameer Hamza <101882275+Hamz1Ameer@users.noreply.github.com>
2023-11-10 13:50:55 -0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ExpenseTracker from "./ExpenseTracker/ExpenseTracker";
+import PostApp from "./Project2/PostApp";
+const App = () => {
+  return (
+    <div>
+      {/* <ExpenseTracker /> */}
+      <PostApp />
+    </div>
+  );
+};
+
+export default App;
