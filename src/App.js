04bb826ca25b316eba96da28c512f06dbe4cf485
Mohammed Ameer Hamza <ameerhamza7702@gmail.com>
2024-03-26 14:52:17 -0700

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ExpenseTracker from "./ExpenseTracker/ExpenseTracker";
 import PostApp from "./Project2/PostApp";
+import Portfolio from "./Project3/Portfolio";
 const App = () => {
   return (
     <div>
+    <Portfolio/>
       {/* <ExpenseTracker /> */}
-      <PostApp />
+      {/* <PostApp /> */}
     </div>
   );
 };
